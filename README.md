a917cbf342ac6ad5746f2cd0e7b047f46b4646a5
luccaki <lucca.ianaguivara@gmail.com>
2022-04-04 15:16:38 -0300

    first tests

---
+++ README.md
@@ -0,0 +1,6 @@
+# Introduction to Tensorflow 2
+
+conda create -n tf2 python=3.8
+conda activate tf2
+pip install --upgrade pip
+pip install -r ./requirements.txt
---
+++ test.py
@@ -0,0 +1,65 @@
+import tensorflow
+import matplotlib.pyplot as plt
+import numpy as np
+from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, BatchNormalization, GlobalAvgPool2D
+from deeplearning_models import functional_model, MyCustomModel
+
+# tensorflow.keras.Sequential
+model = tensorflow.keras.Sequential(
+    [
+        Input(shape=(28,28,1)),
+        Conv2D(32,(3,3), activation='relu'),
+        Conv2D(64, (3,3), activation='relu'),
+        MaxPool2D(),
+        BatchNormalization(),
+
+        Conv2D(128, (3,3), activation='relu'),
+        MaxPool2D(),
+        BatchNormalization(),
+
+        GlobalAvgPool2D(),
+        Dense(64, activation='relu'),
+        Dense(10, activation='softmax')
+    ]
+)
+
+def display_some_examples(examples, labels):
+    plt.figure(figsize=(10,10))
+    for i in range(25):
+        idx = np.random.randint(0, examples.shape[0]-1)
+        img = examples[idx]
+        label = labels[idx]
+
+        plt.subplot(5,5, i+1)
+        plt.title(str(label))
+        plt.tight_layout()
+        plt.imshow(img, cmap='gray')
+
+    plt.show()
+
+if __name__ == '__main__':
+    (x_train, y_train), (x_test, y_test) = tensorflow.keras.datasets.mnist.load_data()
+
+    print("x_train.shape = ", x_train.shape)
+    print("y_train.shape = ", y_train.shape)
+    print("x_test.shape = ", x_test.shape)
+    print("y_test.shape = ", y_test.shape)
+
+    #display_some_examples(x_train,y_train)
+
+    x_train = x_train.astype('float32') / 255
+    x_test = x_test.astype('float32') / 255
+
+    x_train = np.expand_dims(x_train, axis=-1)
+    x_test = np.expand_dims(x_test, axis=-1)
+    
+    y_train = tensorflow.keras.utils.to_categorical(y_train, 10)
+    y_test = tensorflow.keras.utils.to_categorical(y_test, 10)
+
+    #model = functional_model()
+    #model = MyCustomModel()
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics='accuracy')
+
+    model.fit(x_train, y_train, batch_size=64, epochs=3, validation_split=0.2)
+    # 0.8 train, 0.2 validation, then test
+    model.evaluate(x_test, y_test, batch_size=64)
